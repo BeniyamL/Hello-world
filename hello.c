9a419f3d9eb8fd42f3621c1b9bbe4b8d2643a074
Beniyam Legesse <lbeniyam@gmail.com>
2021-02-13 01:20:07 -0800

    my first push from my local machine

---
+++ hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point for the program
+ *
+ * Return: always success
+*/
+
+int main(void)
+{
+        printf("really vi is better than emacs\n");
+        printf("A wonderfull linux editor\n");
+}
+
